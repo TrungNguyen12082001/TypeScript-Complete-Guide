5d273540d5d08f38be4273410f246c3c4e080d15
TrungNguyen <nguyentattrung1208@gmail.com>
2023-05-24 22:37:37 +0700

    Strictly Type A Complex Object

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,54 @@
-// import { Roles } from "./roles";
+type TypeOfLibrary = "national" | "acadamic" | "public";
 
-enum Roles {
-    admin = "admin",
-    author = "author",
-    editor = "editor"
-}
+type Book = {
+    title: string;
+    pages: number;
+    isbn: string;
+};
 
-type Person = {
+type Member = {
     name: string;
-    email: string;
-    password: string;
-    role: Roles;
+    phone: string;
+    [key: string]: string;
 };
 
-const person: Person = {
-    name: "John",
-    email: "john@email.com",
-    password: "password",
-    role: Roles.editor
+type Library = {
+    name: string;
+    address: string;
+    numberOfBooks: number;
+    type: TypeOfLibrary;
+    books: Book[];
+    genres: string[];
+    members: Member[];
 };
 
-console.log(person);
+const library: Library = {
+    name: "NYC",
+    address: "23 some street, NY",
+    numberOfBooks: 1245,
+    type: "national",
+    books: [
+        {
+            title: "Harry",
+            pages: 756,
+            isbn: "9910-2-0123-0"
+        },
+        {
+            title: "Davinci",
+            pages: 386,
+            isbn: "9971-923-3-12"
+        }
+    ],
+    genres: ["fiction", "history", "computers", "poetry"],
+    members: [
+        {
+            name: "John Doe",
+            phone: "+1645352"
+        },
+        {
+            name: "Mark Doe",
+            phone: "+12332424234",
+            email: "mark@email.com"
+        }
+    ]
+};

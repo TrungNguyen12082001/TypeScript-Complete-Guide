51482ae67be8b105209f503d8536c9c0a476659d
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-02 10:51:22 +0700

    Declaring Functions in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,11 @@
-type TypeOfLibrary = "national" | "acadamic" | "public";
+function intro(name: string, age: number): string {
+    return `My name is ${name} and age is ${age}`;
+}
 
-type Book = {
-    title: string;
-    pages: number;
-    isbn: string;
+const intro2 = function intro(name: string, age: number): string {
+    return `My name is ${name} and age is ${age}`;
 };
 
-type Member = {
-    name: string;
-    phone: string;
-    [key: string]: string;
-};
-
-type Library = {
-    name: string;
-    address: string;
-    numberOfBooks: number;
-    type: TypeOfLibrary;
-    books: Book[];
-    genres: string[];
-    members: Member[];
-};
-
-const library: Library = {
-    name: "NYC",
-    address: "23 some street, NY",
-    numberOfBooks: 1245,
-    type: "national",
-    books: [
-        {
-            title: "Harry",
-            pages: 756,
-            isbn: "9910-2-0123-0"
-        },
-        {
-            title: "Davinci",
-            pages: 386,
-            isbn: "9971-923-3-12"
-        }
-    ],
-    genres: ["fiction", "history", "computers", "poetry"],
-    members: [
-        {
-            name: "John Doe",
-            phone: "+1645352"
-        },
-        {
-            name: "Mark Doe",
-            phone: "+12332424234",
-            email: "mark@email.com"
-        }
-    ]
+const intro3 = (name: string, age: number): string => {
+    return `My name is ${name} and age is ${age}`;
 };

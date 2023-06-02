21fc152ada52d93dc914c82b017670d3bfafae15
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-02 14:52:50 +0700

    Intro to Function Overloading

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,10 @@
-enum AgeUnit {
-    years = "years",
-    months = "months"
-}
-
-type greetingFunction = (greeting: string) => string;
-
-type Person = {
-    name: string;
-    age: number;
-    ageUnit: AgeUnit;
-    country: string;
-    greet: greetingFunction;
+type Reservation = {
+    departureDate: Date;
+    returnDate: Date;
+    departingFrom: string;
+    destination: string;
 };
 
-const person: Person = {
-    name: "Scott",
-    age: 30.5,
-    ageUnit: AgeUnit.years,
-    country: "USA",
-    greet: (greeting) => { return `${greeting} ${person.name}`; }
+type Reverse = {
+    (departureDate: Date, returnDate: Date, departingFrom: string, destination: string): Reservation;
 };
-
-function convertAgeToMonths(person: Person): Person {
-    person.age = person.age * 12;
-    person.ageUnit = AgeUnit.months;
-    return person;
-}
-
-console.log(person.greet("Good Morning"));
-

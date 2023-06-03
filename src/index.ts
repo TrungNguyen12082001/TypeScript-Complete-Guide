3246ca6de12250e3884a53866b54afa9b710aac9
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-03 09:28:34 +0700

    This Keyword Inside Classes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 class Person {
-    name = "John";
-    email = "john@email.com";
+    name: string;
+    email: string;
 
     constructor(name: string, email: string) {
-        console.log("name: " + name);
-        console.log("email: " + email);
+        this.name = name;
+        this.email = email;
     }
 
     greet() {
-        return `Hello John`;
+        return `Hello ${this.name}`;
     }
 }
 
 const person = new Person("John", "john@email.com");
 const person2 = new Person("Mark", "mark@email.com");
+
+console.log(person);
+console.log(person2);
+
+console.log(person.greet());
+console.log(person2.greet());

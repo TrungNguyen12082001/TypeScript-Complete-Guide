59681e75c04afc1975ae34ad64db0a307d9a6931
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-04 09:55:21 +0700

    Using an Accessor - Getter Method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 class Person {
-    private age: number;
+    private _age: number | undefined;
     constructor(protected name: string) { }
 
     public getName() {
         return this.name;
     }
+
+    public set age(age: number) {
+        if (age > 200 || age < 0) {
+            throw new Error("The age must be within range 0-200");
+        }
+        this._age = age;
+    }
+
+    public get age() {
+        if (this._age === undefined) {
+            throw new Error("The age has not been set as yet");
+        }
+        return this._age;
+    }
 }
 
 const person: Person = new Person("John");
-const mark: Person = new Person("Mark");
+person.age = 70;
 
-console.log(person);
-console.log(mark);
+console.log(person.age);

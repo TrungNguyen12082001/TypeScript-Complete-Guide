68ee64afab7e7cda505a519f85a661f70982dffa
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-07 21:27:18 +0700

    Inheriting From Multiple Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+enum Roles {
+    admin = "admin",
+    author = "author",
+    editor = "editor"
+}
+
+interface Role {
+    role: Roles;
+}
+
+enum PermissionsList {
+    read = "read",
+    write = "write",
+    execute = "execute"
+}
+
+interface UserPermissions {
+    permissions: PermissionsList[];
+}
+
+interface AdminUser extends User, Role, UserPermissions {
+    numberOfUsersReporting: number;
+}
+
 interface User {
     name: string;
     email: string;
@@ -24,5 +48,18 @@ const userWithAddress: UserWithAddress = {
     address: "This is an address"
 };
 
-console.log(user);
-console.log(userWithAddress);
+const rob: AdminUser = {
+    name: "Rob",
+    email: "rob@email.com",
+    phone: 9934234,
+    gender: "male",
+    role: Roles.admin,
+    permissions: [
+        PermissionsList.execute,
+        PermissionsList.read,
+        PermissionsList.write
+    ],
+    numberOfUsersReporting: 5
+};
+
+console.log(rob);
